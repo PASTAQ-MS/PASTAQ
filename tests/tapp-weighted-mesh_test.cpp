504bcdfe2742dba5309635d74f4815fb83f7f62b
Alex Sanchez <a.sanchez.brotons@rug.nl>
2018-06-06 17:28:44 +0200

    Change number format for consistency

--- tests/tapp-weighted-mesh_test.cpp
+++ tests/tapp-weighted-mesh_test.cpp
@@ -5,16 +5,16 @@
 
 TEST_CASE("Check if data and weights are being set properly") {
     auto mesh = WeightedMesh({2, 2}, {0.0, 75.0, 200.0, 800.0});
-    mesh.set(0,0,10,10);
-    CHECK(mesh.at(0,0) == 100);
-    CHECK(mesh.weightAt(0,0) == 10);
+    mesh.set(0,0,10.0,10.0);
+    CHECK(mesh.at(0,0) == 100.0);
+    CHECK(mesh.weightAt(0,0) == 10.0);
     CHECK(mesh.countsAt(0,0) == 1);
-    mesh.set(1,1,1,20);
-    CHECK(mesh.at(1,1) == 20);
-    CHECK(mesh.weightAt(1,1) == 20);
+    mesh.set(1,1,1.0,20.0);
+    CHECK(mesh.at(1,1) == 20.0);
+    CHECK(mesh.weightAt(1,1) == 20.0);
     CHECK(mesh.countsAt(1,1) == 1);
-    mesh.set(1,1,2,20);
-    CHECK(mesh.at(1,1) == 60);
-    CHECK(mesh.weightAt(1,1) == 40);
+    mesh.set(1,1,2.0,20.0);
+    CHECK(mesh.at(1,1) == 60.0);
+    CHECK(mesh.weightAt(1,1) == 40.0);
     CHECK(mesh.countsAt(1,1) == 2);
 }
