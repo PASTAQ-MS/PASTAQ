49aca3524da6452ce6898b056fa2482df34f3fd6
Alex Sanchez <a.sanchez.brotons@rug.nl>
2018-06-21 11:39:10 +0200

    Add initial implementation of Grid::File::load_range

--- src/grid_file.cpp
+++ src/grid_file.cpp
@@ -5,12 +5,17 @@
 
 bool Grid::File::load(std::istream &stream, std::vector<double> *destination,
                       Grid::Parameters *parameters) {
+    if (destination == nullptr || parameters == nullptr) {
+        return false;
+    }
     if (Grid::File::load_parameters(stream, parameters)) {
         auto n_points = parameters->dimensions.n * parameters->dimensions.m;
         destination->resize(n_points);
         stream.seekg(0, std::ios::beg);
         stream.read(reinterpret_cast<char *>(&(*destination)[0]),
                     sizeof(double) * n_points);
+    } else {
+        return false;
     }
     return stream.good();
 }
@@ -23,8 +28,76 @@ bool Grid::File::write(std::ostream &stream, const std::vector<double> &source,
     return stream.good();
 }
 
+bool Grid::File::load_range(std::istream &stream, const Grid::Bounds &bounds,
+                            std::vector<double> *destination,
+                            Grid::Parameters *parameters) {
+    if (destination == nullptr || parameters == nullptr) {
+        return false;
+    }
+    Grid::Parameters file_parameters = {};
+    if (Grid::File::load_parameters(stream, &file_parameters)) {
+        // Get the indexes for the sliced range.
+        auto i_min = Grid::x_index(bounds.min_mz, file_parameters);
+        auto i_max = Grid::x_index(bounds.max_mz, file_parameters);
+        auto j_min = Grid::y_index(bounds.min_rt, file_parameters);
+        auto j_max = Grid::y_index(bounds.max_rt, file_parameters);
+
+        // Check that indexes min/max are not swapped.
+        if (i_min > i_max || j_min > j_max) {
+            return false;
+        }
+
+        // Check that indexes are inside the grid.
+        if ((i_min > file_parameters.dimensions.n - 1) ||
+            (j_min > file_parameters.dimensions.m - 1)) {
+            return false;
+        }
+
+        // Get the precise bounds for sliced range.
+        Grid::Bounds sliced_bounds = {};
+        sliced_bounds.min_mz = Grid::mz_at(i_min, file_parameters).value();
+        sliced_bounds.max_mz = Grid::mz_at(i_max, file_parameters).value();
+        sliced_bounds.min_rt = Grid::rt_at(i_min, file_parameters).value();
+        sliced_bounds.max_rt = Grid::rt_at(i_max, file_parameters).value();
+
+        // Get dimensions for sliced range.
+        Grid::Dimensions sliced_dimensions = {};
+        sliced_dimensions.n = (i_max - i_min);
+        sliced_dimensions.m = (j_max - j_min);
+        if (sliced_dimensions.n * sliced_dimensions.m == 0) {
+            return false;
+        }
+        parameters->dimensions = sliced_dimensions;
+        parameters->bounds = sliced_bounds;
+        parameters->smoothing_params = file_parameters.smoothing_params;
+        parameters->flags = file_parameters.flags;
+        parameters->instrument_type = file_parameters.instrument_type;
+
+        auto n_points = parameters->dimensions.n * parameters->dimensions.m;
+        destination->resize(n_points);
+
+        // Fill the destination with the data from the sliced parameters.
+        for (size_t j = j_min; j <= j_max; ++j) {
+            stream.seekg(
+                (j * file_parameters.dimensions.n + i_min) * sizeof(double),
+                std::ios::beg);
+            stream.read(reinterpret_cast<char *>(&(*destination)[0]),
+                        sizeof(double) * parameters->dimensions.n);
+            if (stream.bad()) {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
+    return stream.good();
+}
+
 bool Grid::File::load_parameters(std::istream &stream,
                                  Grid::Parameters *parameters) {
+    if (parameters == nullptr) {
+        return false;
+    }
     // Read the last byte of the stream to determine the length of the footer.
     stream.seekg(-1, std::ios::end);
     stream.seekg(-1 * stream.peek(), std::ios::end);
@@ -40,7 +113,8 @@ bool Grid::File::write_parameters(std::ostream &stream,
     auto footer_size = static_cast<char>(sizeof(Grid::Parameters) +
                                          sizeof(Grid::File::Parameters));
     Grid::File::Parameters file_parameters = {1, footer_size};
-    stream.write(reinterpret_cast<const char *>(&parameters), sizeof(parameters));
+    stream.write(reinterpret_cast<const char *>(&parameters),
+                 sizeof(parameters));
     stream.write(reinterpret_cast<const char *>(&file_parameters),
                  sizeof(file_parameters));
     return stream.good();
--- src/grid_file.hpp
+++ src/grid_file.hpp
@@ -21,25 +21,31 @@ struct Parameters {
 };
 
 // Load an entire file from the binary stream into the destination vector and
-// the parameters structure.
+// the parameters structure. Returns the success or failure of the operation.
 bool load(std::istream &stream, std::vector<double> *destination,
           Grid::Parameters *parameters);
 
 // Write the entire source vector and parameters struct into the destination
-// binary stream.
+// binary stream. Returns the success or failure of the operation.
 bool write(std::ostream &stream, const std::vector<double> &source,
            const Grid::Parameters &parameters);
 
-// Load the parameters from the footer of the binary stream.
-bool load_parameters(std::istream &stream, Grid::Parameters *parameters);
-bool write_parameters(std::ostream &stream, const Grid::Parameters &parameters);
+// Loads a specific range from the given stream. Both the destination and
+// parameters will be properly modified. Returns the success or failure of the
+// operation.
+bool load_range(std::istream &stream, const Grid::Bounds &bounds,
+                std::vector<double> *destination, Grid::Parameters *parameters);
 
 // TODO(alex): Allow for reading or loading specific ranges.
-// bool load_range(std::istream &stream, const Grid::Bounds &bounds,
-// std::vector<double> &destination, Grid::Parameters &parameters);
 // bool write_range(std::ostream &stream, const Grid::Bounds &bounds,
 // const std::vector<double> &source,
 // const Grid::Parameters &parameters);
+
+// Load the parameters from the footer of the binary stream. Returns the success
+// or failure of the operation.
+bool load_parameters(std::istream &stream, Grid::Parameters *parameters);
+bool write_parameters(std::ostream &stream, const Grid::Parameters &parameters);
+
 
 }  // namespace Grid::File
 
