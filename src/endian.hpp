260c4de93b4c222a49ac4863b6d59c02543c649c
Alex Sanchez <a.sanchez.brotons@rug.nl>
2018-06-23 22:04:38 +0200

    Change endian swap code to not be dependant on platform endianness

--- src/endian.hpp
+++ src/endian.hpp
@@ -1,42 +1,47 @@
 namespace Endian {
 
-constexpr bool platform_little_endian() {
-    // NOTE(alex): This is a stub. Right now the code is designed to be used on
-    // a x86_64 machine, which is LITTLE ENDIAN. With C++20 we will be able to
-    // use std::endian::native == std::endian::little. Since currently no good
-    // options exist to portably detect the machine endianness at compile time I
-    // will defer the implementation of this function to the future.
-    return true;
-}
-
 inline uint32_t swap_uint32(uint32_t b, bool little_endian) {
-    if (!platform_little_endian() && !little_endian) {
-        return (b >> 24 & 0xFFFFFFFF) | (b >> 8 & 0x0000FF00) |
-               (b << 8 & 0x00FF0000) | (b << 24 & 0xFF000000);
+    uint64_t ret;
+    uint8_t *data = reinterpret_cast<uint8_t *>(&ret);
+
+    if (little_endian) {
+        data[0] = b >> 24;
+        data[1] = b >> 16;
+        data[2] = b >> 8;
+        data[3] = b >> 0;
+    } else {
+        data[3] = b >> 24;
+        data[2] = b >> 16;
+        data[1] = b >> 8;
+        data[0] = b >> 0;
     }
-    if (platform_little_endian() && little_endian) {
-        return (b << 24 & 0xFFFFFFFF) | (b << 8 & 0x0000FF00) |
-               (b >> 8 & 0x00FF0000) | (b >> 24 & 0xFF000000);
-    }
-    return b;
+    return ret;
 }
 
 inline uint64_t swap_uint64(uint64_t b, bool little_endian) {
-    if (!platform_little_endian() && !little_endian) {
-        uint64_t b1 = (b >> 32) & 0x00000000FFFFFFFF;
-        uint64_t b2 = b & 0x00000000FFFFFFFF;
-        b1 = swap_uint32(b1, little_endian);
-        b2 = swap_uint32(b2, little_endian);
-        return (b2 << 32) | b1;
+    uint64_t ret;
+    uint8_t *data = reinterpret_cast<uint8_t *>(&ret);
+
+    if (little_endian) {
+        data[0] = b >> 56;
+        data[1] = b >> 48;
+        data[2] = b >> 40;
+        data[3] = b >> 32;
+        data[4] = b >> 24;
+        data[5] = b >> 16;
+        data[6] = b >> 8;
+        data[7] = b >> 0;
+    } else {
+        data[7] = b >> 56;
+        data[6] = b >> 48;
+        data[5] = b >> 40;
+        data[4] = b >> 32;
+        data[3] = b >> 24;
+        data[2] = b >> 16;
+        data[1] = b >> 8;
+        data[0] = b >> 0;
     }
-    if (platform_little_endian() && little_endian) {
-        uint64_t b1 = b & 0x00000000FFFFFFFF;
-        uint64_t b2 = (b >> 32) & 0x00000000FFFFFFFF;
-        b1 = swap_uint32(b1, little_endian);
-        b2 = swap_uint32(b2, little_endian);
-        return (b1 << 32) | b2;
-    }
-    return b;
+    return ret;
 }
 
 }  // namespace Endian
