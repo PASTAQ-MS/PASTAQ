bcfb2d38f685be7c6bdf25828e360ce9b2fe07d9
Alex Sanchez <a.sanchez.brotons@rug.nl>
2019-02-01 15:45:22 +0100

    Add python method to RawData object to calculate the total ion chromatogram

--- python-bindings/src/tapp/__init__.py
+++ python-bindings/src/tapp/__init__.py
@@ -1,1 +1,20 @@
+import math
+
 from .tapp import *
+
+def tic(raw_data, min_rt = -math.inf, max_rt = math.inf):
+    rt = []
+    intensity = []
+    for i in range(0, len(raw_data.scans)):
+        if (raw_data.scans[i].retention_time < min_rt or
+            raw_data.scans[i].retention_time > max_rt):
+            continue
+        sum = 0
+        for j in range(0, raw_data.scans[i].num_points):
+            sum = sum + raw_data.scans[i].intensity[j]
+        intensity = intensity + [sum]
+        rt = rt + [raw_data.scans[i].retention_time]
+
+    return (rt, intensity)
+
+RawData.tic = tic
