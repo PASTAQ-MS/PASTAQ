cc64dd93bbc42742ecd8eba2f4bb87eb50bb42b4
Alex Sanchez <a.sanchez.brotons@rug.nl>
2020-01-30 16:50:35 +0100

    Add extra test cases

--- tests/feature_detection_test.cpp
+++ tests/feature_detection_test.cpp
@@ -12,8 +12,13 @@ TEST_CASE("DEBUG") {
         TestUtils::mock_gaussian_peak(4, 100.0, 400.5, 2000.0, 0.001, 10),
         TestUtils::mock_gaussian_peak(5, 22.0, 401.5, 2000.0, 0.001, 10),
         TestUtils::mock_gaussian_peak(6, 3.0, 402.5, 2000.0, 0.001, 10),
-        TestUtils::mock_gaussian_peak(7, 101010.0, 402.0, 2000.0, 0.001, 10),
-        TestUtils::mock_gaussian_peak(8, 101001.0, 402.5, 2000.0, 0.001, 10),
+        TestUtils::mock_gaussian_peak(7, 300.0, 402.0, 2000.0, 0.001, 10),
+        TestUtils::mock_gaussian_peak(8, 100.0, 402.0, 2000.0, 0.001, 10),
+        TestUtils::mock_gaussian_peak(9, 45.0, 402.5, 2000.0, 0.001, 10),
+        TestUtils::mock_gaussian_peak(10, 12.0, 403.0, 2000.0, 0.001, 10),
+        TestUtils::mock_gaussian_peak(11, 2.0, 403.5, 2000.0, 0.001, 10),
+        TestUtils::mock_gaussian_peak(12, 30000.0, 402.0, 2000.0, 0.001, 10),
+        TestUtils::mock_gaussian_peak(13, 30000.0, 402.5, 2000.0, 0.001, 10),
     };
     std::vector<uint8_t> charge_states = {2, 1};
     FeatureDetection::find_candidates(peaks, charge_states);
