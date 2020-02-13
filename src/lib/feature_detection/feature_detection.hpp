b152e23cfaef8516460e48a3689636865aacfcba
Alex Sanchez <a.sanchez.brotons@rug.nl>
2020-02-13 17:43:59 +0100

    Remove MIDAS and dependant functions in feature_detection

--- src/lib/feature_detection/feature_detection.hpp
+++ src/lib/feature_detection/feature_detection.hpp
@@ -41,27 +41,6 @@ struct RootNode {
 
 typedef std::vector<RootNode> CandidateGraph;
 
-TheoreticalIsotopes theoretical_isotopes_peptide(std::string sequence,
-                                                 int8_t charge_state,
-                                                 double min_perc);
-
-// TODO: Too many parameters, probably we want to pass a parameters struct
-// instead.
-std::optional<Feature> build_feature(
-    const std::vector<bool> &peaks_in_use,
-    const std::vector<Centroid::Peak> &peaks,
-    const std::vector<Search::KeySort<double>> &peaks_rt_key,
-    const TheoreticalIsotopes &theoretical_isotopes, double tolerance_rt,
-    double retention_time, double discrepancy_threshold, int8_t charge_state);
-
-std::vector<Feature> feature_detection(
-    const std::vector<Centroid::Peak> &peaks,
-    const RawData::RawData &raw_data_ms2,
-    const IdentData::IdentData &ident_data,
-    const std::vector<Link::LinkedMsms> &link_table_msms,
-    const std::vector<Link::LinkedMsms> &link_table_idents,
-    double discrepancy_threshold);
-
 std::vector<Feature> detect_features(const std::vector<Centroid::Peak> &peaks,
                                      const std::vector<uint8_t> &charge_states);
 
