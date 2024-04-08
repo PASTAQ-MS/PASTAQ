95076d6d1f26cdbf606471a2b999971c7cca3538
Venustiano <39260912+Venustiano@users.noreply.github.com>
2024-04-08 13:06:58 +0200

    Update search.hpp

--- src/lib/utils/search.hpp
+++ src/lib/utils/search.hpp
@@ -2,10 +2,11 @@
 #define UTILS_SEARCH_HPP
 
 #include <vector>
-#include <cstddef>
 
 // This namespace contain functions to perform search on data structures.
 namespace Search {
+
+#include <cstddef>
 
 size_t lower_bound(const std::vector<double> &haystack, double needle);
 
